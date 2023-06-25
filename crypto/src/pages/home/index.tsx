01cc4fc989596313939d17c8d424fb6f31cda34b
0dayig0r <igormedeiros.f@gmail.com>
2023-06-25 11:00:07 -0300

    formatando preços

--- crypto/src/pages/home/index.tsx
+++ crypto/src/pages/home/index.tsx
@@ -1,9 +1,55 @@
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import styles from "./home.module.css";
 
 import { BiSearch } from "react-icons/bi";
 
+interface CoinProps {
+  name: string;
+  delta_24h: string;
+  price: string;
+  sybol: string;
+  volume_24h: string;
+  market_cap: string;
+  formatedPrice: string;
+  formatedMarket: string;
+}
+
+interface DataProps {
+  coins: CoinProps[];
+}
+
 function Home() {
+  const [coins, setCoins] = useState<CoinProps[]>([]);
+
+  useEffect(() => {
+    function getData() {
+      fetch("https://sujeitoprogramador.com/api-cripto/?key=67f9141787211428")
+        .then((res) => res.json())
+        .then((data: DataProps) => {
+          const coinsData = data.coins.slice(0, 15);
+
+          let price = Intl.NumberFormat("pt-BR", {
+            style: "currency",
+            currency: "BRL",
+          });
+
+          const formatResult = coinsData.map((item) => {
+            const formated = {
+              ...item,
+              formatedPrice: price.format(Number(item.price)),
+              formatedMarket: price.format(Number(item.market_cap)),
+            };
+            return formated;
+          });
+
+
+          setCoins(formatResult);
+        });
+    }
+
+    getData();
+  }, []);
   return (
     <main className={styles.container}>
       <form className={styles.form}>
@@ -25,19 +71,19 @@ function Home() {
 
         <tbody id="tbody">
           <tr className={styles.tr}>
-            <td className={styles.td} data-label='Moeda'>
+            <td className={styles.td} data-label="Moeda">
               <Link to="/detail/btc" className={styles.link}>
                 <span className={styles.link}>Bitcoin</span> | BTC
               </Link>
             </td>
-            <td className={styles.tdLabel} data-label='Mercado'>
+            <td className={styles.tdLabel} data-label="Mercado">
               <span className={styles.link}>$ 412213</span>
             </td>
-            <td className={styles.tdLabel} data-label='Preço'>
+            <td className={styles.tdLabel} data-label="Preço">
               <span>$ 412213</span>
             </td>
 
-            <td className={styles.tdProfit} data-label='Volume'>
+            <td className={styles.tdProfit} data-label="Volume">
               <span>+ 50</span>
             </td>
           </tr>
